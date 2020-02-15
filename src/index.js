539c4eb9d0a3b386a59a2aece28e2b8d61f02f56
iamdarshshah <imdarshshah@gmail.com>
2020-02-16 02:43:32 +0530

    Reset functionality added. Redux Counter is ready.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const intialState = {
 
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
 
 const increment = () => ({
   type: INCREMENT,
@@ -20,15 +21,26 @@ const decrement = () => ({
   type: DECREMENT,
 });
 
+const reset = () => ({
+  type: RESET,
+});
+
 const reducer = (state = intialState, action) => {
   if (action.type === INCREMENT) {
     return {
       count: state.count + 1,
     };
   }
+
   if (action.type === DECREMENT) {
     return {
       count: state.count - 1,
+    };
+  }
+
+  if (action.type === RESET) {
+    return {
+      count: 0,
     };
   }
 
@@ -39,14 +51,14 @@ const store = createStore(reducer);
 
 class Counter extends Component {
   render() {
-    const { count, increment, decrement } = this.props;
+    const { count, increment, decrement, reset } = this.props;
     return (
       <main className="Counter">
         <p className="count">{count}</p>
         <section className="controls">
           <button onClick={increment}>Increment</button>
           <button onClick={decrement}>Decrement</button>
-          <button>Reset</button>
+          <button onClick={reset}>Reset</button>
         </section>
       </main>
     );
@@ -57,14 +69,10 @@ const mapStateToProps = state => {
   return state;
 };
 
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators(
-    {
-      increment,
-      decrement,
-    },
-    dispatch,
-  );
+const mapDispatchToProps = {
+  increment,
+  decrement,
+  reset,
 };
 
 const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter);
